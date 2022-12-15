ad1fe5ea12c739950ae4e048e15aa8816c1c7103
chenchaoqun.hi@gmail.com <chenchaoqun.hi@gmail.com>
2022-12-15 18:57:18 +0800

    feat: 测试git记录

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react'
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import logo from './logo.svg'
 import './App.css'
+
+// import LikeButton from './components/LikeButtons'
 
 function App() {
 	return (
 		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
+			{/* <LikeButton /> */}
+			<h1>hello world</h1>
 		</div>
 	)
 }
